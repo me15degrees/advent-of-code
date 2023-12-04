8bc2fb3e34a3217b8d7ea1025f18b96143adce17
maria <105123339+me15degrees@users.noreply.github.com>
2023-12-04 13:02:16 -0300

    Update day1.py
    add coments

--- day1.py
+++ day1.py
@@ -1,59 +1,35 @@
 #PART ONE
-"""with open("input_day1.txt", 'r') as certificate:
-    soma = 0
+with open("input_day1.txt", 'r') as certificate:
+    
+    s = 0 # inicialize variable sum with zero value
 
-    for line in certificate:
-        digits = []
+    for line in certificate: # read every line in the archive
+        digits = [] # inicialize an empty list
 
-        for item in line:
-            if item.isdigit():
-                digits.append(int(item))
+        for item in line: # read every item in the line
+            if item.isdigit(): # verify if item is a number
+                digits.append(int(item)) # add the int version of the item in the list
 
-        if len(digits) == 1:
+        if len(digits) == 1: # if the size is one, it's necessary to duplicate the num - to concatenate!
             num = str(digits[0])
             digit = num+num
-            soma += int(digit)
-        elif len(digits) > 1:
+            s += int(digit) # add them in the sum
+        
+        elif len(digits) > 1: # if the size is more than one, just concatenate the first and last digits
             first = str(digits[0])
             last = str(digits[-1])
             digit = first+last
-            soma += int(digit)
-        print(soma)
-"""
-#PART TWO
-# numbers = ["one","two","three","four","five","six","seven","eight","nine"]
-# with open("input_day1.txt", 'r') as certificate:
+            s += int(digit) # add them in the sum
+        print(s) # the result will appear here
 
-#     soma = 0
-   
-#     lista = []
-#     for line in certificate:
-#         digits_line = []    
-#         line = ''.join(caracter for caracter in line if caracter.isalpha() or caracter.isspace())
-        
-#         line = line.replace("one","o1e")
-#         line = line.replace("two","t2o")
-#         line = line.replace("three","t3e")
-#         line = line.replace("four","f4r")
-#         line = line.replace("five","f5e")
-#         line = line.replace("six","s6x")
-#         line = line.replace("seven","s7n")
-#         line = line.replace("eight","e8t")
-#         line = line.replace("nine","n9e")
-#         for digit in line:
-#             if digit.isdigit():
-#                 digits_line.append(digit)
-#                 print(digits_line)
-                
-      
-
-
+# PART TWO
 with open("input_day1.txt", 'r') as certificate:
-    soma = 0
+    s = 0 # inicialize variable sum with zero value
 
     for line in certificate:
-        digits = []
-        print(line)
+        digits = [] # inicialize an empty list
+        
+        # preparete lines to avoid the bad results of overlapping
         line = line.replace("one","o1e")
         line = line.replace("two","t2o")
         line = line.replace("three","t3e")
@@ -65,17 +41,17 @@ with open("input_day1.txt", 'r') as certificate:
         line = line.replace("nine","n9e")
 
         for item in line:
-            if item.isdigit():
-                digits.append(int(item))
+            if item.isdigit(): # verify if the item is a number
+                digits.append(int(item)) # if so, add in the list!
 
-        if len(digits) == 1:
+        if len(digits) == 1: # concatenate if the size is one
             num = str(digits[0])
             digit = num+num
-        elif len(digits) > 1:
+        
+        elif len(digits) > 1: # concatenate the first and last digit if the size is more than one
             first = str(digits[0])
             last = str(digits[-1])
             digit = first+last
-        print(line)
-        print(digit)
-        soma += int(digit)
-    print(soma)
+        
+        s += int(digit) # sum them all together
+    print(s) # show the result
