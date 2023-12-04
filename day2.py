c9a814045a629632d51a171d8cffbc16f9b95d86
maria <105123339+me15degrees@users.noreply.github.com>
2023-12-04 13:03:00 -0300

    Update day2.py
    add coments

--- day2.py
+++ day2.py
@@ -1,43 +1,45 @@
-# PART 1
-"""total_index = 0
+# PART ONE
+total_index = 0 # sum of indexes start at zero
 
-with open("input_day2.txt", 'r') as tentativas:
-    for line in tentativas:
-        identification, content = line.replace(";", ",").split(":")
-        content = content.split(" ")
-        index = identification.split(" ")[1]
+with open("input_day2.txt", 'r') as attempt:
+    for line in attempt: # read the lines in the archive
+        identification, content = line.replace(";", ",").split(":") # separete the lines in two variables
         
-        ispossible = True
+        content = content.split(" ") # separete content by empty spaces
+        index = identification.split(" ")[1] # index will be the second part of identification, without the word game
+        
+        ispossible = True # create a pass indicator 
         
         for idx, item in enumerate(content):
             item = item.strip()
             if item == "green" or item == "green," or item == "green;":
-                num = int(content[idx - 1])
-                if num > 13:
-                    ispossible = False
+                num = int(content[idx - 1]) # get the number before the appearance of color
+                if num > 13: # if it exceeds the estimated value, it does not count
+                    ispossible = False # so ispossible become False
             elif item == "blue" or item == "blue," or item =="blue;":
-                num = int(content[idx - 1])
-                if num > 14:
-                    ispossible = False
+                num = int(content[idx - 1]) # get the number before the appearance of color
+                if num > 14: # if it exceeds the estimated value, it does not count
+                    ispossible = False # so ispossible become False
             elif item == "red" or item == "red," or item =="red;":
-                num = int(content[idx - 1])
-                if num > 12:
-                    ispossible = False
+                num = int(content[idx - 1]) # get the number before the appearance of color
+                if num > 12: # if it exceeds the estimated value, it does not count
+                    ispossible = False # so ispossible become False
 
-        if ispossible:
+        if ispossible: # counts the times that the variable remained with the value True
             total_index += int(index)
 
-        print("Is possible:", ispossible)
-    print("Total Index:", total_index)
-"""
-# PART 2
-with open("input_day2.txt", 'r') as tentativas:
-    soma = 0
-    for line in tentativas:
-        identification, content = line.replace(";", ",").split(":")
-        content = content.split(" ")
-        index = identification.split(" ")[1]
-        
+    print("total index:", total_index) # show the result
+
+# PART TWO
+with open("input_day2.txt", 'r') as attempt:
+    
+    s = 0
+    
+    for line in attempt:  # read the lines in the archive
+        identification, content = line.replace(";", ",").split(":") # separete the lines in two variables
+        content = content.split(" ") # separete content by empty spaces
+        index = identification.split(" ")[1] # index will be the second part of identification, without the word game
+        # create empty lists for each color
         num_green = []
         num_red = []
         num_blue = []
@@ -46,23 +48,21 @@ with open("input_day2.txt", 'r') as tentativas:
             item = item.strip(",").strip()
 
             if item == "green":
-                num = int(content[idx-1])
-                num_green.append(num)
+                num = int(content[idx-1]) # get the number before the appearance of color
+                num_green.append(num) # add in the list
             elif item == "blue":
-                num = int(content[idx-1])
-                num_blue.append(num)
+                num = int(content[idx-1]) # get the number before the appearance of color
+                num_blue.append(num) # add in the list
             elif item == "red":
-                num = int(content[idx-1])
-                num_red.append(num)
-
-        menor_green = max(num_green, default=1)
-        menor_blue = max(num_blue, default=1)
-        menor_red = max(num_red, default=1)
-        print(menor_green,menor_blue,menor_red)
-        produto = menor_red * menor_green * menor_blue
+                num = int(content[idx-1]) # get the number before the appearance of color
+                num_red.append(num) # add in the list
+        # get the max number for each color
+        max_green = max(num_green, default=1)
+        max_blue = max(num_blue, default=1)
+        max_red = max(num_red, default=1)
         
-
+        product = max_red * max_green * max_blue # multiplicate them
         
-        soma += produto
+        s += product # sum of all multiplications
 
-    print(soma)
+    print(s) # show the result
